688250f05cb89da18c188b67687915569bf2030d
Kanisa1 <k.thiak@alustudent.com>
2024-01-31 19:45:30 +0000

    q1.js

--- C3_G3/q1.js
+++ C3_G3/q1.js
@@ -1,11 +1,14 @@
-function sumOfMultiples(limit) {
-  let sum = 0;
-  for (let i = 0; i < limit; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
+// The sum of all the multiples of 3 or 5 below 1000
+
+function totalnumbers(limit) {
+    let sum = 0;
+    for (let i = 1; i < limit; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
     }
-  }
-  return sum;
+    return sum;
 }
 
-console.log(sumOfMultiples(1000)); // 233168
+console.log("Sum of multiples of 3 or 5 below 1000:", totalnumbers(1000));
+
