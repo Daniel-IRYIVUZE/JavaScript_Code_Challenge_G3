d30b3b5b4145dd1a31994094de7bf7c3bdddbbab
ikalisa1 <ikalisa1@alustudent.com>
2024-01-31 12:04:45 -0800

    q4.js

---
+++ C3_G3/q4.js
@@ -0,0 +1,15 @@
+// The largest palindrome made from the product of two 3-digit numbers
+
+function palindrome(num) {
+    const strNum = num.toString();
+    return strNum === strNum.split("").reverse().join("");
+}
+
+function largestpalindromeproduct() {
+    let largestpalindrome = 0;
+
+    for (let i = 100; i < 1000; i++) {
+        for (let j = 100; j < 1000; j++) {
+            const product = i * j;
+            if (palindrome(product) && product > largestpalindrome) {
+                largestpalindrome = product;
